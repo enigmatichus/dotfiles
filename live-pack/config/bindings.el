f14b55c23f46d7220e0cbd905a426c74960db24a
luca <enigmatichus@gmail.com>
2013-11-06 16:12:28 +0100

    Reconfigured Windmove keybindings

--- live-pack/config/bindings.el
+++ live-pack/config/bindings.el
@@ -7,3 +7,9 @@
 (define-key global-map (kbd "C-c C-v") 'my-scroll-up)
 (define-key global-map (kbd "C-c C-S-v") 'my-scroll-down)
 (define-key global-map (kbd "C-c M-b") 'nrepl-use-ns)
+
+;; windmove keys rebindings to C-x M-<up>,<down>,<left>,<right>
+(define-key global-map (kbd "C-x M-<left>") 'windmove-left)
+(define-key global-map (kbd "C-x M-<right>") 'windmove-right)
+(define-key global-map (kbd "C-x M-<up>") 'windmove-up)
+(define-key global-map (kbd "C-x M-<down>") 'windmove-down)
