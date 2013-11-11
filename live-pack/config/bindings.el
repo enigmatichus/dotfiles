fd35833ab11475151ae260c0122cabd4c87ca58d
luca <enigmatichus@gmail.com>
2013-11-11 01:02:37 +0100

    Changed 'remote scroll' keybinding and solved conflict with run-jruby

--- live-pack/config/bindings.el
+++ live-pack/config/bindings.el
@@ -5,7 +5,7 @@
 ;;(define-key global-map (kbd "C--") 'text-scale-decrease)
 
 (define-key global-map (kbd "C-c C-v") 'my-scroll-up)
-(define-key global-map (kbd "C-c C-S-v") 'my-scroll-down)
+(define-key global-map (kbd "C-c C-f") 'my-scroll-down)
 (define-key global-map (kbd "C-c M-b") 'nrepl-use-ns)
 
 ;; windmove keys rebindings to C-x M-<up>,<down>,<left>,<right>
@@ -13,6 +13,9 @@
 (define-key global-map (kbd "C-x M-<right>") 'windmove-right)
 (define-key global-map (kbd "C-x M-<up>") 'windmove-up)
 (define-key global-map (kbd "C-x M-<down>") 'windmove-down)
+
+;; Solve conflict for run-jruby (was: C-c C-f)
+(define-key global-map (kbd "C-c C-S-v") 'run-jruby)
 
 ;; org-mode capture
 (define-key global-map (kbd "C-c c") 'org-capture)
